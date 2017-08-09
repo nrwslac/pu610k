684a4f4ef55061dd457f8a605cdfff4da290a9d7
Michael C. Browne <mcbrowne@slac.stanford.edu>
2017-08-09 21:29:13 +0000

    Fix up annunciator script to run on ioc-mec-ipimb01.
    git-svn-id: file:///afs/slac/g/pcds/vol2/svn/pcds/epics/trunk/ioc/mec/pu610k/current@28854 ececa60f-9f63-0410-97c7-ea05888eb260

--- iocBoot/ioc-mec-announce/annunciator.py
+++ iocBoot/ioc-mec-announce/annunciator.py
@@ -10,22 +10,32 @@ charge_ok = Pv.Pv("MEC:PFN:CHARGE_OK", initialize=True, monitor=True)
 
 def start_charge(e):
     global scinit, charge_ok
-    print "sc %d" % scinit
     if scinit > 0:
         scinit -= 1
     elif charge_ok.value != 0:
-        os.system("/usr/local/bin/mpg123 -a hw:0,0 charging.mp3")
+        os.system("/opt/mpg123/bin/mpg123 -a hw:0,0 charging.mp3")
+
+def charge_connect(isConnected):
+    global scinit
+    if not isConnected:
+        scinit = 1
 
 def fire(e):
     global finit
-    print "f %d" % finit
     if finit > 0:
         finit -= 1
     else:
-        os.system("/usr/local/bin/mpg123 -a hw:0,0 fire.mp3")
+        os.system("/opt/mpg123/bin/mpg123 -a hw:0,0 fire.mp3")
+
+def fire_connect(isConnected):
+    global finit
+    if not isConnected:
+        finit = 1
 
 fire_pv     = Pv.Pv("MEC:PFN:FIRE",         initialize=True, monitor=fire)
+fire_pv.add_connection_callback(fire_connect)
 charging_pv = Pv.Pv("MEC:PFN:START_CHARGE", initialize=True, monitor=start_charge)
+charging_pv.add_connection_callback(charge_connect)
 
 while True:
      time.sleep(3600)
--- iocBoot/ioc-mec-announce/st.cmd
+++ iocBoot/ioc-mec-announce/st.cmd
@@ -1,5 +1,5 @@
 #!/bin/sh
 export PSPKG_ROOT=/reg/g/pcds/pkg_mgr
-export PSPKG_RELEASE=controls-0.0.1
+export PSPKG_RELEASE=controls-0.1.8
 source $PSPKG_ROOT/etc/set_env.sh
 ./annunciator.py "$@"
